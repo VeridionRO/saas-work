fe26231be9e395c9720363c2e1f7d51b41827a58
Mihai Vinaga <vinaga.mihai@gmail.com>
2013-08-05 23:42:44 +0300

    ssh commit

--- hw1/part1.rb
+++ hw1/part1.rb
@@ -1,4 +1,5 @@
 #For those just starting out, one suggested way to work on your code is to have a command window open and a text editor with this
+# 123
 #file loaded.  Make changes to this file and then run 'ruby part1.rb' at the command line, this will run your program.  Once you're
 #satisfied with your work, save your file and upload it to the checker.
 # 11  11
