8b416dbd8a4c46e15731a18b2e8195dec4405b93
Mihai Vinaga <vinaga.mihai@gmail.com>
2013-08-09 13:19:00 +0300

    Finished hw3

--- hw3/features/step_definitions/movie_steps.rb
+++ hw3/features/step_definitions/movie_steps.rb
@@ -2,19 +2,17 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create! movie
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+And /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  first = page.body.index(e1) or return false
+  second = page.body.index(e2) or return false
+  assert first < second
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -22,7 +20,22 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  ratings_hash = rating_list.split(", ")
+  ratings_hash.each do |checkbox|
+    if uncheck
+      uncheck("ratings[" + checkbox + "]")
+    else
+      check("ratings[" + checkbox + "]")
+    end
+  end
+end
+
+
+And /I should see (no|all)? movies/ do |all|
+  if all == "all"
+    assert page.should have_css "#movies/tbody tr", :count => 10 
+  else
+    assert page.should have_css "#movies/tbody tr", :count => 0 
+  end
+  assert true
 end
